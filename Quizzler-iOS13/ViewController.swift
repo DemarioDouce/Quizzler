f53f841459da6ce7ffe458146e063ab9492118e8
Demario Douce <demario.douce567@gmail.com>
2020-05-18 13:15:44 -0400

    Added code to prevent the app from crashing.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //UI elements
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    //data source
+    let quiz = [
+    ["4 + 2 = 6", "True"],
+    ["5 - 3 > 1", "True"],
+    ["3 + 8 < 10", "False"]
+    ]
+    
+    //counter
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
-
+    //button function
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnsswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnsswer == actualAnswer{
+            print("Right!")
+        }else {
+            print("Wong!")
+        }
+       
+        
+        //Check if the question number is less than quiz array
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            
+            questionNumber = 0
+        }
+        
+         updateUI()
+        
+    }
+    
+    //Update array
+    func updateUI(){
+        
+        questionLabel.text = quiz[questionNumber][0]
+        
+    }
+    
 }
 
